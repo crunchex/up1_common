9ea18b720ae1d0f0a664a47f91e4c34f72a543e4
Mike Lewis <mike@updroid.com>
2015-09-09 13:19:27 -0700

    kinematics: added left and right

---
+++ up1_left_arm_kinematics/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_ikfast create_ikfast_moveit_plugin.py up1 left_arm_group up1_left_arm_kinematics /home/crunchex/uproot/catkin_ws/src/up1_left_arm_kinematics/src/up1_left_arm_group_ikfast_solver.cpp
